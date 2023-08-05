d62164c6e35ada1dcb063884c89533f704c8c762
Eduardo Urias <edw.urias@gmail.com>
2023-08-04 18:56:40 -0700

    Added medium sql confirmation rate mysql version

--- medium/confirmation-rate.sql
+++ medium/confirmation-rate.sql
@@ -1,6 +1,7 @@
 -- Problem: https://leetcode.com/problems/confirmation-rate/
 -- Solution: https://leetcode.com/problems/confirmation-rate/submissions/1012502880/
 
+-- CTE version
 WITH ConfirmationStats AS (
     SELECT
         user_id,
@@ -18,3 +19,10 @@ SELECT
 FROM Signups s
      LEFT JOIN ConfirmationStats c
      USING(user_id);
+
+-- Simplified / MySQL version
+SELECT
+    s.user_id,
+    ROUND(AVG(IF(action='confirmed', 1, 0)), 2) AS confirmation_rate
+FROM Signups s LEFT JOIN Confirmations c USING(user_id)
+GROUP BY s.user_id;
